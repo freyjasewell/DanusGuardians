9cf252592b83b61cd351a23df7f49fb401f63bb4
Francesca <francescagreetham@gmail.com>
2022-09-26 16:55:17 +0100

    Updated Build errors and updated video player URL for Daniel. Now works in WebGL.
    Need to update for all other videos (must be under 25mb)

--- DanuTestFile/Assets/Scripts/CinemachineSequence.cs
+++ DanuTestFile/Assets/Scripts/CinemachineSequence.cs
@@ -1,9 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using UnityEngine.Audio;
-using UnityEditorInternal;
 
 public class CinemachineSequence : MonoBehaviour
 {
